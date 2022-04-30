4b47541022c7ff5a4878b35e15f6e280659cd582
SebsibeTefferi <sebsibetefferi145@gmail.com>
2022-04-30 12:02:15 -0700

    pointers arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 /**
- * *_strncat - like _strcat but it will use n bytes from src
+ * _strncat - a function that concatenates two strings
  * 
- * @dest: string to be concatenated
- * @src: string to be concatenated until n limit
- * @n: number of bytes that will be used from src
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
  * 
- * Return: pointer to the resulting string dest
+ * Return: @dest
  */
 
 char *_strncat(char *dest, char *src, int n)
