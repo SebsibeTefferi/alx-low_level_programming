6c5375e05fbb613d3cfe06dcefd5410580af3786
SebsibeTefferi <sebsibetefferi145@gmail.com>
2022-04-30 12:29:45 -0700

    pointers arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,18 +1,25 @@
-#include "main.h"
 /**
  * reverse_array - reverses content of an array of integers
+ * 
  * @a: array of integers
  * @n: numbers of elements of array
+ * 
  * Return: Always 0 (Success)
  */
+
 void reverse_array(int *a, int n)
 {
-int x, y;
+int temp, s, e;
 
-for (x = 0; x < (n / 2); x++)
+s = 0;
+e = n - 1;
+
+while (s < e)
 {
-y = a[x];
-a[x] = a[n - x - 1];
-a[n - x - 1] = y;
+temp = a[s];
+a[s] = a[e];
+a[e] = temp;
+s++;
+e--;
 }
 }
