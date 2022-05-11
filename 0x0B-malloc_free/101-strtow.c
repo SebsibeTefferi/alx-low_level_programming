cf7219ef52017db75dca4c7e5ca2b5f225776343
SebsibeTefferi <sebsibetefferi145@gmail.com>
2022-05-11 06:19:54 -0700

    task3&6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,7 +17,7 @@ w = 0;
 for (c = 0; s[c] != '\0'; c++)
 {
 if (s[c] == ' ')
-	flag = 0;
+flag = 0;
 else if (flag == 0)
 {
 flag = 1;
@@ -40,14 +40,14 @@ char **matrix, *tmp;
 int i, k = 0, len = 0, words, c = 0, start, end;
 
 while (*(str + len))
-	len++;
+len++;
 words = count_word(str);
 if (words == 0)
-	return (NULL);
+return (NULL);
 
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
-	return (NULL);
+return (NULL);
 
 for (i = 0; i <= len; i++)
 {
@@ -58,9 +58,9 @@ if (c)
 end = i;
 tmp = (char *) malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
-	return (NULL);
+return (NULL);
 while (start < end)
-	*tmp++ = str[start++];
+*tmp++ = str[start++];
 *tmp = '\0';
 matrix[k] = tmp - c;
 k++
@@ -68,7 +68,7 @@ c = 0;
 }
 }
 else if (c++ == 0)
-	start = i;
+start = i;
 }
 
 matrix[k] = NULL;
